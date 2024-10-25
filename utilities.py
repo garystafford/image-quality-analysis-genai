451ef375d34c98a1c99dc1e8c4593b3ce488199c
Gary A. Stafford <gary.a.stafford@gmail.com>
2024-10-24 22:51:38 -0400

    Clean up code with black

--- utilities.py
+++ utilities.py
@@ -22,7 +22,20 @@ logging.basicConfig(
 class Utilities:
     @staticmethod
     def truncate(response: str) -> dict:
-        # Modify the response to include the correct image_id and time
+        """
+        Truncate and parse a JSON response string.
+
+        This function modifies the input response string to ensure it starts and ends with
+        curly braces, then attempts to parse it as a JSON object. If the parsing fails,
+        it returns a dictionary with an error explanation and a score of -1.
+
+        Args:
+            response (str): The raw response string to be truncated and parsed.
+
+        Returns:
+            dict: The parsed JSON object as a dictionary. If parsing fails, returns a
+                  dictionary with an error explanation and a score of -1.
+        """
         try:
             if not response.startswith("{") and response.count("{") > 0:
                 delimiter = "{"
@@ -45,6 +58,16 @@ class Utilities:
 
     @staticmethod
     def resize_image(img: Image, max_pixels: int) -> Image:
+        """
+        Resize an image to ensure its largest dimension does not exceed a specified maximum number of pixels.
+
+        Args:
+            img (Image): The input image to be resized.
+            max_pixels (int): The maximum number of pixels for the largest dimension of the image.
+
+        Returns:
+            Image: The resized image, if resizing was necessary; otherwise, the original image.
+        """
         width, height = img.size
         if max(width, height) > max_pixels:
             scale_factor = max_pixels / max(width, height)
@@ -58,7 +81,19 @@ class Utilities:
 
     @staticmethod
     def image_to_base64(img: Image, file_format: str) -> str:
-        """Convert a PIL Image to bytes"""
+        """
+        Convert a PIL Image to a base64 encoded string.
+
+        Args:
+            img (Image): The PIL Image to be converted.
+            file_format (str): The format to save the image in (e.g., 'JPEG', 'PNG').
+
+        Returns:
+            str: The base64 encoded string representation of the image.
+
+        Raises:
+            ValueError: If the provided img is not an instance of PIL.Image.
+        """
         if isinstance(img, Image.Image):
             logging.debug("Converting PIL Image to bytes")
             buffer = io.BytesIO()
@@ -69,7 +104,19 @@ class Utilities:
 
     @staticmethod
     def image_to_bytes(img: Image, file_format: str) -> bytes:
-        """Convert a PIL Image to bytes"""
+        """
+        Convert a PIL Image to bytes.
+
+        Args:
+            img (Image): The PIL Image to convert.
+            file_format (str): The format to save the image in (e.g., 'JPEG', 'PNG').
+
+        Returns:
+            bytes: The image data in bytes.
+
+        Raises:
+            ValueError: If the provided img is not an instance of PIL Image.
+        """
         if isinstance(img, Image.Image):
             logging.debug("Converting PIL Image to bytes")
             buffer = io.BytesIO()
@@ -80,6 +127,16 @@ class Utilities:
 
     @staticmethod
     def count_scores(scores: dict) -> Counter:
+        """
+        Count the occurrences of each score in the provided dictionary and return them sorted.
+
+        Args:
+            scores (dict): A dictionary containing a list of score dictionaries under the key "scores".
+                           Each score dictionary should have a "score" key.
+
+        Returns:
+            Counter: A Counter object with the counts of each score, sorted by score.
+        """
         scores_counts = [score["score"] for score in scores["scores"]]
         score_counts = Counter(scores_counts)
         sorted_score_count = sorted(score_counts.items())
